e9c31cb795aea61fef5cae6de416365515e0d579
Daniel Rocha <rochadaniel15@gmail.com>
2020-11-30 14:07:35 -0300

    refactor: Separado os tipos de dados

---
+++ types.h
@@ -0,0 +1,57 @@
+#ifndef __TYPES_H__
+#define __TYPES_H__
+
+#define BOX    '#'
+#define WALL   '*'
+#define DOLL   '@'
+#define TARGET '?'
+
+
+enum Keys {
+  CTRL_C = 3,
+  KEY_UP = 65,
+  KEY_DOWN = 66,
+  KEY_LEFT = 67,
+  KEY_RIGHT = 68,
+};
+
+typedef struct object {
+
+  int x, y;
+  char body;
+
+} Object;
+
+typedef struct box {
+  int x, y;
+  int id;
+  char body;
+  bool enable;
+} Box;
+
+typedef struct goals {
+  int lenght;
+  Object *list;
+} Goals;
+
+typedef struct {
+  Box *list;
+  int lenght;
+} Boxes;
+
+typedef struct map {
+  char **field;
+  int width;
+  int height;
+
+} Map;
+
+typedef struct gameState {
+  Boxes *boxes;
+  Map currrentMap;
+  Object *character;
+  bool gameOver;
+  int level;
+} GameState;
+
+#endif // __TYPES_H__
