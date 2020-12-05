79d9bfc946e82feb13f4c0f5e5a7c0017caaee16
Daniel Rocha <rochadaniel15@gmail.com>
2020-12-05 16:06:42 -0300

    feat: Novas cores para texto e o plano de fundo

--- lib/screen.h
+++ lib/screen.h
@@ -29,6 +29,27 @@
 #define ICYAN "\x1b[36;1m"
 #define IWHITE "\x1b[37;1m"
 
+// BG Colors
+#define BGC_BLACK "\x1b[40m"
+#define BGC_RED "\x1b[41m"
+#define BGC_GREEN "\x1b[42m"
+#define BGC_YELLOW "\x1b[43m"
+#define BGC_BLUE "\x1b[44m"
+#define BGC_MAGENTA "\x1b[45m"
+#define BGC_CYAN "\x1b[46m"
+#define BGC_WHITE "\x1b[47m"
+
+// BG Intense Colors
+#define BGC_IBLACK "\x1b[40;1m"
+#define BGC_IRED "\x1b[41;1m"
+#define BGC_IGREEN "\x1b[42;1m"
+#define BGC_IYELLOW "\x1b[43;1m"
+#define BGC_IBLUE "\x1b[44;1m"
+#define BGC_IMAGENTA "\x1b[45;1m"
+#define BGC_ICYAN "\x1b[46;1m"
+#define BGC_IWHITE "\x1b[47;1m"
+
+
 
 void nocursor() {
   printf("\x1b[?25l");
