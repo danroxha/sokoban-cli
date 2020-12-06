3cdea27a1d265eb09dc91d862bdb46f30defe3f7
Daniel Rocha <rochadaniel15@gmail.com>
2020-12-06 18:16:43 -0300

    refactor: Alterado a função que verificar se o puzzle foi completo

--- logic.h
+++ logic.h
@@ -5,7 +5,7 @@
 #include "lib/types.h"
 
 bool checkCollisionBetweenBoxes(Boxes*);
-void checkPuzzleSolution(GameState*);
+bool checkPuzzleSolution(GameState*);
 void handleBoxesOnTarget(Boxes*, Goals*);
 
 
@@ -23,7 +23,7 @@ bool checkCollisionBetweenBoxes(Boxes *boxes) {
   return false;
 }
 
-void checkPuzzleSolution(GameState *gameState) {
+bool checkPuzzleSolution(GameState *gameState) {
   
   int completePuzzle = 0;
   
@@ -32,7 +32,7 @@ void checkPuzzleSolution(GameState *gameState) {
       completePuzzle++;
   }
 
-  gameState->gameOver = completePuzzle == gameState->boxes->lenght;
+  return completePuzzle == gameState->boxes->lenght;
 
 }
 
